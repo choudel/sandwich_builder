d1fa52778c436394ac4b5a471dce5f4254c067a6
Choudel <61978194+choudel@users.noreply.github.com>
2023-05-18 16:10:32 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	p {
+		color: blue;
+		@media only screen and (max-width: $sm) {
+			color: brown;
+		}
+	}
+</style>
