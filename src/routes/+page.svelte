bd21706a45b11e2f9ebdcdd02b3b0c36d220be50
Choudel <61978194+choudel@users.noreply.github.com>
2023-05-19 08:31:48 +0100

    absolute first layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="hero">Sandwich final shape</header>
+<section class="info">infos on the ingredient</section>
+<section class="pick">
+	<nav class="category">choose type of an ingredient</nav>
+	<div class="options">
+		<div class="card">card1</div>
+		<div class="card">card2</div>
+		<div class="card">card3</div>
+		<div class="card">card4</div>
+	</div>
+</section>
 
 <style lang="scss">
-	p {
-		color: blue;
+	header {
+		display: grid;
+		background: #888282;
 		@media only screen and (max-width: $sm) {
-			color: brown;
+			min-height: 76vmax;
+		}
+
+		@media only screen and (max-width: $md) {
+			min-height: 45vmax;
+		}
+		@media only screen and (max-width: $lg) {
+			min-height: 45vmax;
+		}
+	}
+	.info {
+		display: grid;
+		background: #595050;
+		@media only screen and (max-width: $sm) {
+			min-height: 5vmax;
+		}
+		@media only screen and (max-width: $md) {
+			min-height: 10vmax;
+		}
+		@media only screen and (max-width: $lg) {
+			min-height: 12vmax;
+		}
+	}
+	.pick {
+		display: grid;
+		background: #d8d1d1;
+		@media only screen and (max-width: $sm) {
+			min-height: 19vmax;
+		}
+		@media only screen and (max-width: $md) {
+			min-height: 45vmax;
+		}
+		@media only screen and (max-width: $lg) {
+			min-height: 43vmax;
 		}
 	}
 </style>
